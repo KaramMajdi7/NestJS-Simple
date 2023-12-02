5e4352705ccaea45b05abdd497e52050220ec92e
KaramMajdi7 <karam.majdi@gmail.com>
2023-12-02 13:53:14 +0300

    First Push 🪄

---
+++ src/chat.gateway.ts
@@ -0,0 +1,21 @@
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+
+@WebSocketGateway({
+    cors: {
+      origin: 'http://127.0.0.1:5500',
+      methods: ['GET', 'POST'],
+      transports: ['websocket', 'polling'],
+      credentials: true,
+    },
+    allowEIO3: true,
+  })
+export class ChatGateway {
+
+    @WebSocketServer()
+    server;
+    
+    @SubscribeMessage('message')
+    handleMessage(@MessageBody() message: string): void {
+        this.server.emit('message', message);
+    }
+}
